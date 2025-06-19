dccfe69210ed3ed9d292346eb003d3e466fcb364
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-06-20 04:26:50 +0900

    feat: Docker環境構築

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config: { watchOptions: { poll: number; aggregateTimeout: number; }; }) => {
+    config.watchOptions = {
+      poll: 60,
+      aggregateTimeout: 60,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
